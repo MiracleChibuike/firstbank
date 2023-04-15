c41b7ccc79eb75037b05ebefdc378b9ec061c5b3
MiracleChibuike <miraclechibuike27@gmail.com>
2023-04-15 08:39:45 -0700

    firstbank

---
+++ js/index.js
@@ -0,0 +1,62 @@
+
+function phone() {
+    let version;
+    // school = prompt("where are you from")
+    version = [2.0, 2.1, 4.0,  5,  6.1, 7.0, 8.0, 9.0, 10.0, 11.0, 14];
+    if (version <=2.5) {
+        document.getElementById("phoneversion").innerHTML="Version 2.0 - Cupcake";
+        document.getElementById("phoneversion").style.color = 'white';
+        document.getElementById("phoneversion").style.fontSize = '15';
+        document.getElementById("phoneversion").style.textAlign = "center"
+    }else if (version <=2.1){
+        document.getElementById("phoneversion").innerHTML="Version 2.1 - Eclair";
+        document.getElementById("phoneversion").style.color = 'white';
+        document.getElementById("phoneversion").style.fontSize = '15';
+        document.getElementById("phoneversion").style.textAlign = "center";
+    }else if (version <=2.1){
+        document.getElementById("phoneversion").innerHTML="Version 4.0 - Ice-cream sandwhich";
+        document.getElementById("phoneversion").style.color = 'white';
+        document.getElementById("phoneversion").style.fontSize = '15';
+        document.getElementById("phoneversion").style.textAlign = "center";
+    }else if (version <=2.1){
+        document.getElementById("phoneversion").innerHTML="Version 5 - Lollilop";
+        document.getElementById("phoneversion").style.color = 'white';
+        document.getElementById("phoneversion").style.fontSize = '15';
+        document.getElementById("phoneversion").style.textAlign = "center";
+    }else if (version <=2.1){
+        document.getElementById("phoneversion").innerHTML="Version 6.1 - Marshmallow";
+        document.getElementById("phoneversion").style.color = 'white';
+        document.getElementById("phoneversion").style.fontSize = '15';
+        document.getElementById("phoneversion").style.textAlign = "center";
+    }else if (version <=2.1){
+        document.getElementById("phoneversion").innerHTML="Version 7.0 - Nougat";
+        document.getElementById("phoneversion").style.color = 'white';
+        document.getElementById("phoneversion").style.fontSize = '15';
+        document.getElementById("phoneversion").style.textAlign = "center";
+    }else if (version <=2.1){
+        document.getElementById("phoneversion").innerHTML="Version 8.0 - Oreo";
+        document.getElementById("phoneversion").style.color = 'white';
+        document.getElementById("phoneversion").style.fontSize = '15';
+        document.getElementById("phoneversion").style.textAlign = "center";
+    }else if (version <=2.1){
+        document.getElementById("phoneversion").innerHTML="Version 9.0 - Pie";
+        document.getElementById("phoneversion").style.color = 'white';
+        document.getElementById("phoneversion").style.fontSize = '15';
+        document.getElementById("phoneversion").style.textAlign = "center";
+    }else if (version <=2.1){
+        document.getElementById("phoneversion").innerHTML="Version 10.0 - Android 10";
+        document.getElementById("phoneversion").style.color = 'white';
+        document.getElementById("phoneversion").style.fontSize = '15';
+        document.getElementById("phoneversion").style.textAlign = "center";
+    }else if (version <=2.1){
+        document.getElementById("phoneversion").innerHTML="Version 11.0 - Android 11";
+        document.getElementById("phoneversion").style.color = 'white';
+        document.getElementById("phoneversion").style.fontSize = '15';
+        document.getElementById("phoneversion").style.textAlign = "center";
+    }else if (version <=2.1){
+        document.getElementById("phoneversion").innerHTML="Version 14 - iOS";
+        document.getElementById("phoneversion").style.color = 'white';
+        document.getElementById("phoneversion").style.fontSize = '15';
+        document.getElementById("phoneversion").style.textAlign = "center";
+    }
+}
